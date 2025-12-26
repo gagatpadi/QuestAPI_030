716477f3ea625a90bc324d5727c64aa20e108184
ASUS <padiyogaradhana@gmail.com>
2025-12-26 18:28:26 +0700

    EntryViewModel.kt/ fun perubahan

--- app/src/main/java/com/example/questapi_030/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_030/viewmodel/EntryViewModel.kt
@@ -20,5 +20,11 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): View
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+    //Fungsi untuk menangani saat ada perubahan pada text input
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
 
 }
