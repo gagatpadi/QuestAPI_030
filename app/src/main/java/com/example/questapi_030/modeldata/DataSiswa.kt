f45746603dd65e1bd995b5f08ea96edd246338dc
ASUS <padiyogaradhana@gmail.com>
2025-12-26 18:09:54 +0700

    DataSiswa.kt data siswa

---
+++ app/src/main/java/com/example/questapi_030/modeldata/DataSiswa.kt
@@ -0,0 +1,12 @@
+package com.example.questapi_030.modeldata
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
+
