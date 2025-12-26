ddf0c9b58f5ac340cd5d2133f503129c96e570f1
ASUS <padiyogaradhana@gmail.com>
2025-12-26 18:28:41 +0700

    EntryViewModel.kt/ fun menyimpan data

--- app/src/main/java/com/example/questapi_030/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_030/viewmodel/EntryViewModel.kt
@@ -26,5 +26,16 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): View
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
-
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            val sip:Response<Void> = repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa
+                .toDataSiswa())
+            if (sip.isSuccessful){
+                println("Sukses Tambah Data : ${sip.message()}")
+            }else{
+                println("Gagal tambah data : ${sip.errorBody()}")
+            }
+        }
+    }
 }
