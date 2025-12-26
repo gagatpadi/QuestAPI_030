79dd17cee148f5258f6e264c50fd2e428f28827f
ASUS <padiyogaradhana@gmail.com>
2025-12-26 18:17:31 +0700

    DetailViewModel.kt/ sealed

---
+++ app/src/main/java/com/example/questapi_030/viewmodel/DetailViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.questapi_030.viewmodel
+
+import android.annotation.SuppressLint
+import android.net.http.HttpException
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_030.modeldata.DataSiswa
+import com.example.questapi_030.repositori.RepositoryDataSiswa
+import com.example.questapi_030.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
