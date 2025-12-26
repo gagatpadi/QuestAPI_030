e56ecdb1e70352bf56be868c362677ab08b85d8e
ASUS <padiyogaradhana@gmail.com>
2025-12-26 18:12:29 +0700

    DestinasiDetail.kt

---
+++ app/src/main/java/com/example/questapi_030/uicontroller/route/DestinasiDetail.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_030.uicontroller.route
+
+import com.example.questapi_030.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+}
