0e9df2a6e3c9f9b559acc05b442c02ec3816eb03
ASUS <padiyogaradhana@gmail.com>
2025-12-26 18:13:14 +0700

    DestinasiEdit.kt

---
+++ app/src/main/java/com/example/questapi_030/uicontroller/route/DestinasiEdit.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_030.uicontroller.route
+
+import com.example.questapi_030.R
+
+object DestinasiEdit: DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+}
