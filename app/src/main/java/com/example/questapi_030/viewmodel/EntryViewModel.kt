e472d5faba75e33ee3f13587c16e72addd152489
ASUS <padiyogaradhana@gmail.com>
2025-12-26 18:27:14 +0700

    EntryViewModel.kt/ class

---
+++ app/src/main/java/com/example/questapi_030/viewmodel/EntryViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.questapi_030.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.questapi_030.modeldata.DetailSiswa
+import com.example.questapi_030.modeldata.UIStateSiswa
+import com.example.questapi_030.modeldata.toDataSiswa
+import com.example.questapi_030.repositori.RepositoryDataSiswa
+import retrofit2.Response
+
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+
+}
